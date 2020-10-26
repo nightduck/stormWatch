8e62e67a97ca52562c5c1f84a6d482afa47cd31b
oren <virtualoren@gmail.com>
2020-10-25 21:42:28 -0500

    Repairs

--- iot_test_publish/publish.py
+++ iot_test_publish/publish.py
@@ -8,9 +8,9 @@ import AWSIoTPythonSDK.MQTTLib as AWSIoTPyMQTT
 # Define ENDPOINT, CLIENT_ID, PATH_TO_CERT, PATH_TO_KEY, PATH_TO_ROOT, MESSAGE, TOPIC, and RANGE
 ENDPOINT = "a2hdizbqiesak7-ats.iot.us-east-2.amazonaws.com"
 CLIENT_ID = "testDevice"
-PATH_TO_CERT = "certificates/29707f2f05-certificate.pem.crt"
-PATH_TO_KEY = "certificates/29707f2f05-private.pem.key"
-PATH_TO_ROOT = "certificates/root.pem"
+PATH_TO_CERT = "../data/29707f2f05-certificate.pem.crt"
+PATH_TO_KEY = "../data/29707f2f05-private.pem.key"
+PATH_TO_ROOT = "../data/root.pem"
 MESSAGE = "Hello World! This is our very first IoT test message!"
 TOPIC = "test/testing"
 RANGE = 10
