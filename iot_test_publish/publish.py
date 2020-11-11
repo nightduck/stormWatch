b7b104af6f21b43593fc1dd267eacf6fc43909f5
oren <virtualoren@gmail.com>
2020-11-10 23:31:32 -0600

    Tweaks to test script

--- iot_test_publish/publish.py
+++ iot_test_publish/publish.py
@@ -6,13 +6,13 @@ import json
 import AWSIoTPythonSDK.MQTTLib as AWSIoTPyMQTT
 
 # Define ENDPOINT, CLIENT_ID, PATH_TO_CERT, PATH_TO_KEY, PATH_TO_ROOT, MESSAGE, TOPIC, and RANGE
-ENDPOINT = "a2hdizbqiesak7-ats.iot.us-east-2.amazonaws.com"
-CLIENT_ID = "testDevice"
-PATH_TO_CERT = "../data/29707f2f05-certificate.pem.crt"
-PATH_TO_KEY = "../data/29707f2f05-private.pem.key"
-PATH_TO_ROOT = "../data/root.pem"
-MESSAGE = "Hello World! This is our very first IoT test message!"
-TOPIC = "test/testing"
+ENDPOINT = "ENDPOINT.iot.us-east-2.amazonaws.com"
+CLIENT_ID = "THINGNAME"
+PATH_TO_CERT = "../data/FILENAME.pem.crt"
+PATH_TO_KEY = "../data/FILENAME.pem.key"
+PATH_TO_ROOT = "../data/FILENAME.pem"
+MESSAGE = {"state":{"reported":{"temp":23.54,"pressure":997.5694,"humidity":55.86035,"wind_direction":"NW","wind_speed":4.5,"rainfall_mm":2.3,"battery":3.902637,"timestamp":1605071283}}}
+TOPIC = "THINGNAME/weather"
 RANGE = 10
 
 myAWSIoTMQTTClient = AWSIoTPyMQTT.AWSIoTMQTTClient(CLIENT_ID)
@@ -22,10 +22,9 @@ myAWSIoTMQTTClient.configureCredentials(PATH_TO_ROOT, PATH_TO_KEY, PATH_TO_CERT)
 myAWSIoTMQTTClient.connect()
 print('Begin Publish')
 for i in range (RANGE):
-    data = "{} [{}]".format(MESSAGE, i+1)
-    message = {"message" : data}
-    myAWSIoTMQTTClient.publish(TOPIC, json.dumps(message), 1)
-    print("Published: '" + json.dumps(message) + "' to the topic: " + "'test/testing'")
+    MESSAGE["state"]["reported"]["timestamp"] += 111 * i
+    myAWSIoTMQTTClient.publish(TOPIC, json.dumps(MESSAGE), 1)
+    print("Published: '" + json.dumps(MESSAGE) + "' to the topic: " + "'test/testing'")
     t.sleep(0.1)
 print('Publish End')
 myAWSIoTMQTTClient.disconnect()
